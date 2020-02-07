1695fad14403999b9e4274739de550482ce44f9f
Tianqi <airstone42@gmail.com>
2020-02-07 17:36:06 +0800

    :sparkles: apply model changes

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ import yaml
 from bs4 import BeautifulSoup
 
 CONF_PATH = 'config.yml'
-IMG_PATH = 'data/'
-MODEL_PATH = 'data/model.h5'
-NUM_PATH = 'data/number.csv'
+LEFT_PATH = 'data/left.h5'
+RIGHT_PATH = 'data/right.h5'
+NUM_PATH = 'data/numbers.csv'
 
 DK_URL = 'https://dk.shmtu.edu.cn/'
 CAS_URL = 'https://cas.shmtu.edu.cn/'
@@ -23,15 +23,19 @@ CAPTCHA_URL = CAS_URL + 'cas/captcha'
 CHECKIN_URL = DK_URL + 'checkin'
 
 df = pd.read_csv(NUM_PATH)
-x_mean = np.array(ast.literal_eval(df['mean'][0]))
-x_std = np.array(ast.literal_eval(df['std'][0]))
-model = keras.models.load_model(MODEL_PATH)
+xl_mean, xr_mean = np.array(ast.literal_eval(df['l_mean'][0])), np.array(ast.literal_eval(df['r_mean'][0]))
+xl_std, xr_std = np.array(ast.literal_eval(df['l_std'][0])), np.array(ast.literal_eval(df['r_std'][0]))
+l_model, r_model = keras.models.load_model(LEFT_PATH), keras.models.load_model(RIGHT_PATH)
 
 
 def recognize(img) -> str:
-    img = (img - x_mean) / x_std
-    img = img[..., np.newaxis]
-    return str(model.predict_classes([img])[0]).zfill(2)
+    _, width = img.shape
+    l_part: np.ndarray = img[..., :int(width / 2)]
+    r_part: np.ndarray = img[..., int(width / 2):]
+    l_part, r_part = (l_part - xl_mean) / xl_std, (r_part - xr_mean) / xr_std
+    l_part = l_part[..., np.newaxis]
+    r_part = r_part[..., np.newaxis]
+    return str(l_model.predict_classes(l_part)[0]) + str(r_model.predict_classes(r_part)[0])
     pass
 
 
@@ -90,11 +94,13 @@ def checkin(s: requests.Session, r: requests.Response, config: dict) -> bool:
 
 def main():
     if not os.path.exists(CONF_PATH):
-        print('Config does not exist!')
+        logging.warning('Config does not exist!')
+        return
     with open(CONF_PATH, 'r') as f:
         config = yaml.load(f, Loader=yaml.Loader)
         if not config['id'] and config['number']:
-            print('Config incorrect!')
+            logging.warning('Config incorrect!')
+            return
     error_count = 0
 
     s = requests.Session()
